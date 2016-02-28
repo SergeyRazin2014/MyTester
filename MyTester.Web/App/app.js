778172539f3abf573793e5ecc12fe2b7fbdd76fc
SergeyRazin2014 <900556@mail.ru>
2016-02-28 19:23:48 +0400

    loading bar

--- MyTester.Web/App/app.js
+++ MyTester.Web/App/app.js
@@ -1,4 +1,4 @@
-﻿var app = angular.module('app', ['ngRoute', 'ui.bootstrap', 'ngAnimate']);
+﻿var app = angular.module('app', ['ngRoute', 'ui.bootstrap', 'ngAnimate', 'angular-loading-bar']);
 
 app.config(function ($routeProvider) {
 
